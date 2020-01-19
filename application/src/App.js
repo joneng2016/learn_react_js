16287ad0d508f599d733702f5b256974fe0a3bbc
joneng2016 <jonathan_samara@hotmail.com>
2020-01-19 13:28:06 -0200

    fazer o próximo, que eh sobre States

--- application/src/App.js
+++ application/src/App.js
@@ -8,8 +8,17 @@ import Hello from "./components/Hello";
 class App extends Component {
   render() {
     return (
-      <div class="App">           
-        <Hello />
+      <div className="App">           
+        <Greet name="Bruce" heroName="Batman">
+          <p>This is Children Props</p>
+        </Greet>
+        <Greet name="Clark" heroName="SuperMan">
+          <button>Action</button>
+        </Greet>
+        <Greet name="Diana" heroName="Wonder Woman"/>
+        <Welcome name="Bruce" heroName="Batman" />
+        <Welcome name="Clark" heroName="SuperMan" />
+        <Welcome name="Diana" heroName="Wonder Woman" />                        
       </div>
     );
   }
--- application/src/components/Hello.js
+++ application/src/components/Hello.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 const Hello = () => {
-//    return (
-//        <div>
-//            <h1>Hello Vishwas</h1>
-//        </div>
-//    )
-    return React.createElement('div', null, React.createElement('h1',null,'Helo'));
+
+    return React.createElement(
+        'div', 
+        {id:"hello", className:"dummyClass"}, 
+        React.createElement('h1',null,'Hello')
+    );
+    
 }
 
 export default Hello;
